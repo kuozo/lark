d6cc824946e7e584f0b0dc161f7b9c6b0ccb271d
Kollin <kollinchu@gmail.com>
2018-06-21 17:33:33 +0800

    update script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ export CGO_ENABLED=0
 export GOOS=linux
 export GOARCH=amd64
 
-go build -ldflags '-extldflags "-static"' -a -v -o ../bin/linux/lark ./cmd/
+go build -ldflags '-extldflags "-static"' -a -v -o bin/linux/lark ./cmd/
